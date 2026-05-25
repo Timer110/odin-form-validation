57a772314268cebdd249fa2ecb1403f1ad10db8b
Timer110 <121953691+Timer110@users.noreply.github.com>
2026-05-25 16:19:42 +0200

    implemented all Validation-rules and styling via :valid/:invalid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './style.css';
 
-console.log("Hello World!");
+const $signupForm = document.querySelector('[data-js="signup-form"]');
+const $inputEmail = $signupForm.querySelector('[data-js="input-signup-email"]');
+const $inputPassword = $signupForm.querySelector('[data-js="input-signup-password"]');
+const $inputPasswordConfirm = $signupForm.querySelector('[data-js="input-signup-password-confirm"]');
+const $inputPostalcode = $signupForm.querySelector('[data-js="input-signup-postalcode"]');
+const $inputCountry = $signupForm.querySelector('[data-js="input-signup-country"]');
+const $buttonSubmit = $signupForm.querySelector('[data-js="signup-submit"]');
+const inputs = [$inputEmail, $inputPassword, $inputPasswordConfirm, , $inputPostalcode, $inputCountry];
+
+//Form-validation functions
+
+function validateField (field) {
+    if (field.validity.typeMismatch) {
+        field.setCustomValidity('Falsch!')
+    } else {
+        field.setCustomValidity('');
+    };
+
+    if (field.validity.valueMissing) {
+        field.setCustomValidity('Bitte alle Felder ausfüllen!');
+    } else {
+        field.setCustomValidity('');
+    };
+
+    if (field.validity.patternMismatch) {
+        field.setCustomValidity('Postleitzahl falsch!')
+    } else {
+        field.setCustomValidity('');
+    }
+}; 
+
+inputs.forEach(input => {
+    input.addEventListener('blur', () => {
+        input.dataset.touched = true;
+        validateField(input);
+    });
+})
+
+//Special for equal Passwords
+
+$inputPasswordConfirm.addEventListener('blur', (e) => {
+    if ($inputPassword.value !== $inputPasswordConfirm.value) {
+        $inputPasswordConfirm.setCustomValidity('Passwörter stimmen nicht überein!');
+    } else {
+        $inputPasswordConfirm.setCustomValidity('');
+    }
+});
